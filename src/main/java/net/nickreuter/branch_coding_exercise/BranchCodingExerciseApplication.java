8c3f8a68c3cba4d05c69d656879a971ae3390485
Nick Reuter <thenewimagineer@gmail.com>
2024-12-18 22:31:34 -0500

    Initial commit

---
+++ src/main/java/net/nickreuter/branch_coding_exercise/BranchCodingExerciseApplication.java
@@ -0,0 +1,13 @@
+package net.nickreuter.branch_coding_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchCodingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchCodingExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/nickreuter/branch_coding_exercise/BranchCodingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nickreuter.branch_coding_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchCodingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
